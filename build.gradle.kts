e750ec74e441bb8aab4a9c4c8f79f267edbc7ce6
defconradio <linux_self@yahoo.com>
2026-01-27 01:43:11 -0400

    alias

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.graalvm.native.image) apply false
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.multiplatform")
-    id("com.android.library")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.serialization)
 }
 
