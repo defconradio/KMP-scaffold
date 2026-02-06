202b340fd871b466a478bb2b99ce168ef2b551e8
richborg <ricardobessil@gmail.com>
2026-02-06 18:36:11 -0400

    ddd

---
+++ iosApp/kmp_scaffold/kmp_scaffold/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kmp_scaffold
+//
+//  Created by Ricardo Bessil on 6/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/kmp_scaffold/kmp_scaffold/kmp_scaffoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  kmp_scaffoldApp.swift
+//  kmp_scaffold
+//
+//  Created by Ricardo Bessil on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct kmp_scaffoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
